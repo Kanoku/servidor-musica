21a74040d74a1f04c8356ee77af9289c4ab22df0
Jonatan Granados <joni2granados@gmail.com>
2018-04-16 11:42:15 +0200

    Añadida funcionalidad de descarga y botones en la web

--- app.js
+++ app.js
@@ -37,6 +37,25 @@ app.get('/music', function(req,res){
 	});
 });
 
+app.get('/download', function(req,res){
+	var fileId = req.query.id;
+	var file = __dirname + '/music/' + fileId;
+	fs.exists(file,function(exists){
+		if(exists)
+		{
+			res.setHeader('Content-disposition', 'attachment; filename=' + fileId);
+			res.setHeader('Content-Type', 'application/audio/mpeg3')
+			var rstream = fs.createReadStream(file);
+			rstream.pipe(res);
+		}
+		else
+		{
+			res.send("Its a 404");
+			res.end();
+		}
+	});
+});
+
 // start app on port 3003 and log the message to console
 
 app.listen(3003,function(){
